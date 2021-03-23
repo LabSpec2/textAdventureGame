d96fa0d81cf0ac3f3f478d84f6a0444f51a9c308
sroczyk <karolina.sroczyk@nokia.com>
2021-03-23 23:15:26 +0100

    nadanie impienia oraz cech postaci

--- page.py
+++ page.py
@@ -1,31 +1,45 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
+import random
 
 class Stats:
     def __init__(self):
         self.strength = 0
+        self.dexterity = 0
+        self.constitution = 0
         self.wisdom = 0
-        self.speed = 0
+        self.charisma = 0
     
-    def add(self, add_str=0, add_wisdom=0, add_speed=0):
-        self.strength += add_str
+    def add(self, add_strength=0, add_dexterity=0, add_constitution=0, add_wisdom=0, add_charisma=0):
+        self.strength += add_strength
+        self.dexterity += add_dexterity
+        self.constitution += add_constitution
         self.wisdom += add_wisdom
-        self.speed += add_speed
+        self.charisma = add_charisma
 
 class Hero:
-    def __init__(self, name):
+    hero_id_counter = 0
+    def __init__(self,  name = "User"):
+        self.hero_id = Hero.hero_id_counter
+        Hero.hero_id_counter = Hero.hero_id_counter + 1
         self.name = name
         self.stats = Stats()
-        
-    def upgrade(self, stat_name, points):
-        if  stat_name == "strength":
-            self.stats.add(add_str=points)
-        elif stat_name == "wisdom":
-            self.stats.add(add_wisdom=points)
-        else:
-            self.stats.add(add_speed=points)
-            
+        self.HP = random.randint(20, 35)
+
+def upgrade(self, stat_name, points):
+    if stat_name == "strength":
+        self.stats.add(add_strength=points)
+    elif stat_name == "dexterity":
+        self.stats.add(add_dexterity=points)
+    elif stat_name == "constitution":
+        self.stats.add(add_constitution=points)
+    elif stat_name == "wisdom":
+        self.stats.add(add_wisdom=points)
+    else:
+        self.stats.add(add_charisma=points)
+
 app = Flask(__name__)
-temp_hero = Hero(name="Jakub Wędrowycz")
+temp_hero = Hero(name = "Wedrownik")
+upgrade(temp_hero, stat_name="strength", points=int(5))
 
 @app.route("/")
 def home():
@@ -37,7 +51,7 @@ def story():
     
 @app.route("/story/add/<ile>")
 def ile(ile):
-    temp_hero.upgrade(stat_name="strength",points=int(ile))
+    upgrade(temp_hero, stat_name="strength",points=int(ile))
     return render_template("story_template.html", hero=temp_hero)
 
 if __name__ == "__main__":
