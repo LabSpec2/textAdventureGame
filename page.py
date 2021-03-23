5cebc906431483b20e3837e98e9067a6e5cd7567
patrycjaszubart <patrycjaszubart@gmail.com>
2021-03-24 00:25:19 +0100

    tbd

--- page.py
+++ page.py
@@ -1,45 +1,63 @@
 from flask import Flask, redirect, url_for, render_template, request
 import random
+import enum
+from random import randrange
 
 class Stats:
-    def __init__(self):
-        self.strength = 0
-        self.dexterity = 0
-        self.constitution = 0
-        self.wisdom = 0
-        self.charisma = 0
+    def __init__(self, skill_dict = {}):
+        self.strength = skill_dict["strength"]
+        self.dexterity = skill_dict["dexterity"]
+        self.constitution = skill_dict["constitution"]
+        self.wisdom = skill_dict["wisdom"]
+        self.charisma = skill_dict["charisma"]
+        
+    def add(self, skill_dict):
+        self.strength += skill_dict["strength"]
+        self.dexterity += skill_dict["dexterity"]
+        self.constitution += skill_dict["constitution"]
+        self.wisdom += skill_dict["wisdom"]
+        self.charisma += skill_dict["charisma"]
+        
+class MonsterType(enum.Enum):
+	Vampire = 1
+	Ghost = 2
+	Ghoul = 3
+	Zombie = 4
+	Skeleton = 5
     
-    def add(self, add_strength=0, add_dexterity=0, add_constitution=0, add_wisdom=0, add_charisma=0):
-        self.strength += add_strength
-        self.dexterity += add_dexterity
-        self.constitution += add_constitution
-        self.wisdom += add_wisdom
-        self.charisma = add_charisma
-
+class Monster:
+    def __init__(self, monster_type, attack_max, hp_max):
+        self.type = type
+        self.attack_max = attack_max
+        self.hp_max = hp_max
+    
+    def getModifier():
+        return 0 # dict tbd
+    
+    def getAttack():
+        return 10 + randrange(attack_max) - getModifier()
+        
 class Hero:
-    hero_id_counter = 0
+    id_counter = 0
     def __init__(self,  name = "User"):
-        self.hero_id = Hero.hero_id_counter
-        Hero.hero_id_counter = Hero.hero_id_counter + 1
+        self.id= Hero.id_counter
+        Hero.id_counter = Hero.id_counter + 1
         self.name = name
         self.stats = Stats()
         self.HP = random.randint(20, 35)
+        self.attack_range = attack_range
+        
+        self.hero_attack_mod = {MonsterType.Vampire: self.stats.charisma, MonsterType.Ghost: self.stats.wisdom} # tbd
 
-def upgrade(self, stat_name, points):
-    if stat_name == "strength":
-        self.stats.add(add_strength=points)
-    elif stat_name == "dexterity":
-        self.stats.add(add_dexterity=points)
-    elif stat_name == "constitution":
-        self.stats.add(add_constitution=points)
-    elif stat_name == "wisdom":
-        self.stats.add(add_wisdom=points)
-    else:
-        self.stats.add(add_charisma=points)
-
+    def upgrade(self, skill_dict):
+        self.stats.add(skill_dict)
+           
+    def getAttack(monster_attacking):
+        return 10 + randrange(attack_range) + self.hero_attack_mod(monster_attacking.type)
+		
+            
 app = Flask(__name__)
-temp_hero = Hero(name = "Wedrownik")
-upgrade(temp_hero, stat_name="strength", points=int(5))
+temp_hero = Hero(name = "Wedrownik", attack_range=5)
 
 @app.route("/")
 def home():
@@ -48,10 +66,11 @@ def home():
 @app.route("/story/")
 def story():
     return render_template("story_template.html", hero=temp_hero)
-    
+   
+#temp route   
 @app.route("/story/add/<ile>")
 def ile(ile):
-    upgrade(temp_hero, stat_name="strength",points=int(ile))
+    temp_hero.upgrade({"strength" : 7})
     return render_template("story_template.html", hero=temp_hero)
 
 if __name__ == "__main__":
