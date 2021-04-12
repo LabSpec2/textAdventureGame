fef226cd0e8641419e0f48d74542d7f57d45c445
patrycjaszubart <patrycjaszubart@gmail.com>
2021-04-12 21:08:52 +0200

    added stats

--- page.py
+++ page.py
@@ -1,84 +1,149 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
+import random
+import enum
+import copy
+from random import randrange
+from collections import defaultdict
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
 
-inv = ['broken umbrella','copper spoon','ashen-wood stake','garlick','torn pillow','broken rake','revolver with silver bullets','porcelain plate','apple','lighter','small stick']
-_name='Harry Dresden'
-_hp=30
-_stats=[5,4,5,3,3]
-@app.route("/story/")
-def story():
-    return render_template("story_template.html", inventory = inv,name = _name,hp=_hp,stat=_stats)
-    
-@app.route("/<_name>")
-def user(_name):
-    return render_template("story_template.html", name = _name)
-	
-@app.route("/hero/")
-def hero():
-    return render_template("hero.html", points = 25)
-    
-    
-if __name__ == "__main__":
-    app.run(debug=True)
-'''
-
-#broken umbrella,ashen-wood stake,torn pillow,apple,garlick,lighter,broken rake,porcelain plate,copper spoon,revolver with silver bullets,small stick<br>
-
-from flask import Flask, redirect, url_for, render_template, request
-import random
+def makeDict(dict_):
+    to_return = defaultdict(int)
+    for key, value in dict_.items():
+        to_return[key] = value
+    return to_return
 
 class Stats:
-    def __init__(self):
+    def __init__(self): 
         self.strength = 0
         self.dexterity = 0
         self.constitution = 0
         self.wisdom = 0
         self.charisma = 0
+        
+    def add(self, dict_, max_=10):
+        self.strength =  min(max_, self.strength + dict_["strength"])
+        self.dexterity = min(max_, self.dexterity + dict_["dexterity"])
+        self.constitution = min(max_, self.constitution + dict_["constitution"])
+        self.wisdom = min(max_, self.wisdom + dict_["wisdom"])
+        self.charisma = min(max_, self.charisma + dict_["charisma"])
+           
+    def getByName(self, name):
+        if name == "strength":
+            return self.strength
+        if name == "dexterity":
+            return self.dexterity
+        if name == "constitution":
+            return self.constitution
+        if name == "wisdom":
+            return self.wisdom
+        if name == "charisma":
+            return self.charisma     
+        raise Exception('Unsupported stat: {}'.format(name))
+         
+        
+class MonsterType(enum.Enum):
+	Vampire = 15
+	Ghost = 12
+	Ghoul = 8
+	Zombie = 6
+	Skeleton = 1
     
-    def add(self, add_strength=0, add_dexterity=0, add_constitution=0, add_wisdom=0, add_charisma=0):
-        self.strength += add_strength
-        self.dexterity += add_dexterity
-        self.constitution += add_constitution
-        self.wisdom += add_wisdom
-        self.charisma = add_charisma
 
+monster_hp = {MonsterType.Vampire:30, MonsterType.Ghost:18, MonsterType.Ghoul:12,\
+              MonsterType.Zombie:9, MonsterType.Skeleton:3}  
+
+monster_modifier = {MonsterType.Vampire:"dexterity", MonsterType.Ghost:"wisdom",\
+                    MonsterType.Ghoul:"constitution", MonsterType.Zombie:"constitution",\
+                    MonsterType.Skeleton:"dexterity"}
+
+hero_modifier = {MonsterType.Vampire:"charisma", MonsterType.Ghost:"wisdom",\
+                    MonsterType.Ghoul:"strength", MonsterType.Zombie:"dexterity",\
+                    MonsterType.Skeleton:"strength"}
+                    
+item_stats = {"WoodenBat":{"dexterity":5,"constitution":4,"charisma":3},\
+              "ThinStick":{"dexterity":2,"constitution":3,"charisma":4},\
+              "Garlic":{"dexterity":0,"constitution":0,"charisma":7},\
+              "Salt":{"dexterity":0,"constitution":0,"charisma":0},\
+              "Aspen-Wood Stake":{"dexterity":0,"constitution":1,"charisma":9},\
+              "Revolver":{"dexterity":4,"constitution":2,"charisma":5},\
+              "Apple":{"dexterity":0,"constitution":1,"charisma":0},\
+              "EmptyCan":{"dexterity":1,"constitution":2,"charisma":0},\
+              "BrokenUmbrella":{"dexterity":1,"constitution":2,"charisma":0},\
+              "PlantPot":{"dexterity":1,"constitution":1,"charisma":1},\
+              "SmallPainting":{"dexterity":2,"constitution":3,"charisma":1},\
+              "OldShoe":{"dexterity":2,"constitution":1,"charisma":0},\
+              "Pillow":{"dexterity":0,"constitution":5,"charisma":0}}
+
+
+class Monster:
+    def __init__(self, type):
+        self.type = type
+        self.HP = monster_hp[type]
+        self.dead = False
+
+    def getAttack(self, hero_attacking):
+        return 10 + randrange(self.type) - hero_attacking.stats.getByName(monster_modifier[self.type])
+        
 class Hero:
-    hero_id_counter = 0
-    def __init__(self,  name = "User"):
-        self.hero_id = Hero.hero_id_counter
-        Hero.hero_id_counter = Hero.hero_id_counter + 1
+    id_counter = 0
+    def __init__(self,  name = "User",  attack_range=5):
+        self.id= Hero.id_counter
+        Hero.id_counter = Hero.id_counter + 1
         self.name = name
         self.stats = Stats()
         self.HP = random.randint(20, 35)
+        self.attack_range = attack_range
+        self.collected_items = []
+        self.dead = False
 
-def upgrade(self, stat_name, points):
-    if stat_name == "strength":
-        self.stats.add(add_strength=points)
-    elif stat_name == "dexterity":
-        self.stats.add(add_dexterity=points)
-    elif stat_name == "constitution":
-        self.stats.add(add_constitution=points)
-    elif stat_name == "wisdom":
-        self.stats.add(add_wisdom=points)
-    else:
-        self.stats.add(add_charisma=points)
+    def upgrade(self, skill_dict):
+        self.stats.add(skill_dict)
+    
+    def addItem(self, item):
+        self.collected_items.append(item)
+        #dict_ = makeDict(item_stats[item])
+        #self.stats.add(dict_)
+        
+    def getAttack(self, monster_attacking):    
+        return 10 + randrange(self.attack_range) + self.stats.getByName(hero_modifier[monster_attacking])
 
-app = Flask(__name__)
-temp_hero = Hero(name = "Wedrownik")
-upgrade(temp_hero, stat_name="strength", points=int(5))
+    def fight(self, item, monster_attacking):
+        dict_ = makeDict(item_stats[item])
+        new_stats = copy.deepcopy(self.stats)
+        new_stats.add(item_stats[item])
+        
+        while True:
+            self.HP -=  monster_attacking.getAttack(self)
+            if not self.HP > 0:
+                self.dead = True
+                return
+            monster_attacking.HP -= self.getAttack(monster_attacking)
+            if not monster_attacking.HP > 0:
+                monster_attacking.dead = True
+                return
+        
 
 @app.route("/")
 def home():
-    return render_template("index.html", hero=temp_hero)
+    return render_template("index.html")
+
+hero_ = Hero('Harry Dresden')
+hero_.upgrade(makeDict({"charisma":8, "strength":5}))
 
 @app.route("/story/")
 def story():
-    return render_template("story_template.html", hero=temp_hero)
+    return render_template("story_template.html", inventory = item_stats,hero=hero_)
+	
+@app.route("/hero/")
+def hero():
+    return render_template("hero.html", points = 25)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+
+'''
     
 @app.route("/story/add/<ile>")
 def ile(ile):
@@ -99,7 +164,4 @@ def data():
         return render_template("index.html", hero=temp_hero)
         #return render_template('data.html', form_data=form_data)
 
-#//////////////////////////////////////////////////
-if __name__ == "__main__":
-    app.run(debug=True)
-    '''
+'''
